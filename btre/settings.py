0842b6f3c7817423525e1d990dd8f839404dd893
Andrew Lim <2000andrewlim@gmail.com>
2020-12-23 23:25:36 -0800

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = '3f12aa56cb@gmail.com'
 EMAIL_HOST_PASSWORD = 'roGhie5ooY'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
